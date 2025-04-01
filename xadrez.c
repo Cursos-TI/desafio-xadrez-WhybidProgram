571926dee40331dd664fe9b8165319d4e9de70a3
Whybid Teodoro <twhybid@gmail.com>
2025-04-01 23:23:51 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int torre, bispo = 0, rainha = 0;
+    int torre, bispo = 0, rainha = 0, cavalo = 1;
     
     //estrutura para movimento da torre 5 casas para a direita
     for (torre = 0; torre < 5; torre++)
     {
         printf("Direita\n");
     }
+
+    printf("\n");
     // estrutura para o movimento do bispo 5 casas na diagonal para a direita
     while (bispo < 5)
     {
@@ -15,6 +17,7 @@ int main() {
         bispo++;
     }
 
+    printf("\n");
     //estrutura para o movimento da rainha 8 casas para a esquerda
 
     do
@@ -22,7 +25,16 @@ int main() {
         printf("Esquerda\n");
         rainha++;
     } while (rainha < 8);
-    
 
-    return 0;
-}
+    printf("\n");
+    
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda");
+        
+    }
+    
